56848df25575a8619d3d4f4c6f3b5fb1892bbea5
Shrey <shrey14099@iiitd.ac.in>
2015-04-18 14:16:05 +0530

    Theme integration

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,20 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( landing_page.js )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( set.css )
+Rails.application.config.assets.precompile += %w( blueimp-gallery.min.css )
+
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( jquery.blueimp-gallery.min.js )
+Rails.application.config.assets.precompile += %w( respond.js )
+Rails.application.config.assets.precompile += %w( touchSwipe.min.js )
+Rails.application.config.assets.precompile += %w( wow.min.js )
+Rails.application.config.assets.precompile += %w( script.js )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
